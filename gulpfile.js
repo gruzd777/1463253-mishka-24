46504b7a103ac9e0bd776c0f0ab4d3dec0216bb4
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2022-01-30 20:35:44 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ const optimizeImages = () => {
 }
 
 const copyImages = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(gulp.dest('build/img'))
 }
 
